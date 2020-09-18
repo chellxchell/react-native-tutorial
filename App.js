f9bf92774305c3c297744448d8045c819aba9c9d
Chelly Compendio <rochellecompendio2021@u.northwestern.edu>
2020-09-18 14:17:01 -0500

    fetch url

--- App.js
+++ App.js
@@ -1,31 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
   
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
 
 const getCourseNumber = course => (
   course.id.slice(1)
@@ -48,10 +23,25 @@ const CourseList = ({ courses }) => (
 );
 
 const Banner = ({ title }) => (
-  <Text style={styles.banner}>{ title }</Text>
+  <Text style={styles.bannerStyle}>{ title || '[loading...]' }</Text>
 );
 
+
 const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule =  async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+  
   return (
     <SafeAreaView style={styles.container}>
       <Banner title={schedule.title} />
@@ -59,15 +49,14 @@ const App = () => {
     </SafeAreaView>
   );
 };
-  
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 20,
   },
-  banner: {
+  bannerStyle: {
     color: '#888',
     fontSize: 32,
   },
@@ -79,6 +68,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
   courseButton: {
+    flex: 1,
     borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
