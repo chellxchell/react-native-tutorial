d88f9cc2ab228493dcdd0937c90b30b7611f74e0
Chelly Compendio <rochellecompendio2021@u.northwestern.edu>
2020-10-09 01:23:00 -0500

    login

--- App.js
+++ App.js
@@ -6,20 +6,59 @@ import ScheduleScreen from './screens/ScheduleScreen';
 import CourseDetailScreen from './screens/CourseDetailScreen';
 import CourseEditScreen from './screens/CourseEditScreen';
 import UserContext from './UserContext';
+import { Button } from 'react-native';
+import SignInScreen from './screens/SignInScreen';
+import {firebase} from './firebase';
 
+const SignInButton = ({ navigation, user }) => (
+  user && user.uid
+    ? <Button title="Logout" color="#448aff"
+      onPress={() => firebase.auth().signOut()}
+    />
+    : <Button title="SignIn" color="#448aff"
+      onPress={() => navigation.navigate('SignInScreen')}
+    />
+);
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [user, setUser] = useState({ role: 'admin'});
+  const [user, setUser] = useState({ role: 'admin' });
+  const [auth, setAuth] = useState();
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((auth) => {
+      setAuth(auth);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (auth && auth.uid) {
+      const db = firebase.database().ref("users").child(auth.uid);
+      const handleData = (snap) => {
+        setUser({ uid: auth.uid, ...snap.val() });
+      };
+      db.on("value", handleData, (error) => alert(error));
+      return () => {
+        db.off("value", handleData);
+      };
+    } else {
+      setUser(null);
+    }
+  }, [auth]);
   return (
     <UserContext.Provider value={user}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="ScheduleScreen"
             component={ScheduleScreen}
-            options={{ title: 'Schedule' }}
+            options={({ navigation }) => ({
+              title: "Schedule",
+              headerRight: () => (
+                <SignInButton navigation={navigation} user={user} />
+              ),
+            })
+            }
           />
           <Stack.Screen name="CourseDetailScreen"
             component={CourseDetailScreen}
@@ -29,6 +68,7 @@ const App = () => {
             component={CourseEditScreen}
             options={{ title: 'Course Editor' }}
           />
+          <Stack.Screen name="SignInScreen" component={SignInScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserContext.Provider>
